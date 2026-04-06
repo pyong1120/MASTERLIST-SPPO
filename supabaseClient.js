1a446f1a75a5da98ec8bfd463ffd224c66e503ec
pyong1120 <perezfionagrace@gmail.com>
2026-04-06 14:21:42 +0800

    first commit

---
+++ supabaseClient.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://qdysjglyidwpgdniioy.supabase.co'
+const supabaseKey = 'sb_publishable_LKZi-EPdAF-zIFvrOjfzEQ_mpZLQQJm'
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
